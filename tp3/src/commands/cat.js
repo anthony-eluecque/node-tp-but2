67d142d633bec6bcd3b02a83da0f45f1ec56f5a3
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-03-10 15:31:43 +0100

    cat command with api call

---
+++ tp3/src/commands/cat.js
@@ -0,0 +1,14 @@
+const { SlashCommandBuilder, Integration } = require('discord.js');
+const { request } = require('undici')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('cat')
+        .setDescription('print'),
+    async execute(interaction) {
+        const catResult = await request('https://aws.random.cat/meow')
+        const { file } = await catResult.body.json()
+                
+        await interaction.reply({ files: [file] })
+    },
+}
