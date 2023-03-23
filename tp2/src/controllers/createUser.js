b8b08fd31c408ece609a0d4a44658b97a9bffee8
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-03-23 17:19:07 +0100

    Update createuser

--- tp2/src/controllers/createUser.js
+++ tp2/src/controllers/createUser.js
@@ -1,5 +1,23 @@
+const { schema_user } = require("../repositories/models/user");
+const {Schema} = require('mongodb');
+const { insertOne } = require("../services/db/crud");
+const { generateUUID } = require("../repositories/utils/uuid");
+const { Validator, validate } = require("jsonschema");
+
 const createUser = (req,res) => {
-    res.send("test");
+    user = {
+        "id":generateUUID(),
+        "nom":req.query.nom,
+        "prenom":req.query.prenom,
+        "mail":req.query.mail,
+        "array-watchlists":[]
+    }
+    let v = new Validator();
+    if (v.validate(user,schema_user).valid){
+        insertOne('template',user);
+        return res.status(200).json(user);
+    }
+    return res.status(400).send("erreur");
 }
 
 module.exports = createUser
