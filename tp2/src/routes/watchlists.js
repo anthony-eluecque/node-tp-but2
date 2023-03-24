6123f0916fd4766a7da5ab4e4046529510ffde13
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-03-24 12:55:30 +0100

    update routes

--- tp2/src/routes/watchlists.js
+++ tp2/src/routes/watchlists.js
@@ -3,11 +3,13 @@ const router = Router()
 const express = require('express');
 const { addItemToWatchlist } = require('../controllers/addItemToWatchlist');
 const createWatchlist = require('../controllers/createWatchlist');
+const editStateOfItem = require('../controllers/editStateOfItem');
 
 router.use(express.json());
 router.use(express.urlencoded({ extended: false }))
 
 router.post('/create',createWatchlist)
 router.post('/add',addItemToWatchlist)
+router.post('/edit',editStateOfItem)
 
 module.exports = router;
