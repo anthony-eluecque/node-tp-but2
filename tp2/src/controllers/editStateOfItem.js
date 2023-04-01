664fe7327d6656b6e6a941f67b3ec23e5c32483c
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-04-01 17:44:16 +0200

    Update method with clean code

--- tp2/src/controllers/editStateOfItem.js
+++ tp2/src/controllers/editStateOfItem.js
@@ -1,16 +1,25 @@
 const { findOne, updateOne } = require("../services/db/crud");
 const states = require("../repositories/models/states");
-
 const conf = require("../../conf.json");
+
+const watchlists = conf.tables.watchlists;
 
 const editStateOfItem = async (req,res) => {
 
     try{
+        console.log(req.query);
+        console.log(req.body);
+        if (states.includes(req.body.state)){
+            const watchlist = {'nom':req.body.nomWatchlist,'array-items.id': req.body.idItem};
+            const updated = {$set:{'array-items.$.state':req.body.state}};
+            result = await updateOne(watchlists,watchlist,updated);
+            return res.status(200).json(await findOne(watchlists,{'nom':req.body.nomWatchlist}));        
+        }
         if (states.includes(req.query.state)){
             const watchlist = {'id':req.query.idWatchlist,'array-items.id': req.query.idItem}
             const updated = {$set:{'array-items.$.state':req.query.state}};
-            result = await updateOne('template',watchlist,updated);
-            return res.status(200).json(await findOne('template',{'id':req.query.idWatchlist}));
+            result = await updateOne(watchlists,watchlist,updated);
+            return res.status(200).json(await findOne(watchlists,{'id':req.query.idWatchlist}));
         }
         return res.status(200).send('Conflits, l etat n existe pas');
     }
