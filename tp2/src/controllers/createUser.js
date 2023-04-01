d8efbdb2d1e9635d53c7d8b1192ab991070dae17
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-04-01 17:43:13 +0200

    Update create user with body and query

--- tp2/src/controllers/createUser.js
+++ tp2/src/controllers/createUser.js
@@ -8,13 +8,28 @@ const conf = require("../../conf.json");
 const users = conf.tables.users;
 
 const createUser = (req,res) => {
-    user = {
-        "id":generateUUID(),
-        "nom":req.query.nom,
-        "prenom":req.query.prenom,
-        "mail":req.query.mail,
-        "array-watchlists":[]
+    console.log(req.query);
+    console.log(req.body);
+
+    if (req.query){
+        user = {
+            "id":generateUUID(),
+            "nom":req.query.nom,
+            "prenom":req.query.prenom,
+            "mail":req.query.mail,
+            "array-watchlists":[]
+        }
     }
+    if (req.body){
+        user = {
+            "id":generateUUID(),
+            "nom":req.body.nom,
+            "prenom":req.body.prenom,
+            "mail":req.body.mail,
+            "array-watchlists":[]
+        }     
+    }
+    // console.log(user);
     let v = new Validator();
     if (v.validate(user,schema_user).valid){
         insertOne(users,user);
