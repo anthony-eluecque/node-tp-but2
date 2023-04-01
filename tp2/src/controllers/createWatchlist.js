4b59df9afe5c42dc100c5331044cf4678971676c
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-04-01 17:43:27 +0200

    Create watchlist method with body and query

--- tp2/src/controllers/createWatchlist.js
+++ tp2/src/controllers/createWatchlist.js
@@ -11,22 +11,38 @@ const watchlists = conf.tables.watchlists;
 
 const createWatchlist = async (req,res) => {
     try{
-        watchlist = {
-            "id":generateUUID(),
-            "nom":req.query.nom,
-            "id-proprietaire":req.query.proprietaire,
-            "array-items":[]
+        let user = await findOne(users,{'nom':req.query.proprietaire});
+        console.log(user);
+        if (user){
+            watchlist = {
+                "id":generateUUID(),
+                "nom":req.query.nom,
+                "id-proprietaire":user['id'],
+                "array-items":[]
+            }
         }
+        user = await findOne(users,{'prenom':req.body.proprietaire});
+        console.log(user);
+        if (user){
+            watchlist = {
+                "id":generateUUID(),
+                "nom":req.body.nom,
+                "id-proprietaire":user['id'],
+                "array-items":[]
+            } 
+        }
+
 
         let v = new Validator();
         if (v.validate(watchlist,schema_watchlist).valid){
-            
-            const filter = {'id':req.query.proprietaire};
-            const updated = {$push:{'array-watchlists':watchlist.id}};
+            console.log(watchlist['id']);
+            const filter = {'id':watchlist['id-proprietaire']};
+            console.log(filter);
+            const updated = {$push:{'array-watchlists':watchlist['id']}};
 
             await updateOne(users,filter,updated);
             await insertOne(watchlists,watchlist);
-            result = await findOne(users,{'id':req.query.proprietaire});
+            result = await findOne(users,{'id':watchlist['id-proprietaire']});
             return res.status(200).json(result);
         }
         return res.status(400).send("Conflits");
