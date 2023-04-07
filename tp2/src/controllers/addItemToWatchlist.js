0cc96f6716625ad350de38ebfcd5381cd88a94eb
Antorakk <95444282+Antorakk@users.noreply.github.com>
2023-04-07 14:25:02 +0200

    add feat for tp4

--- tp2/src/controllers/addItemToWatchlist.js
+++ tp2/src/controllers/addItemToWatchlist.js
@@ -10,13 +10,26 @@ const watchlists = conf.tables.watchlists;
 const addItemToWatchlist = async (req,res) => {
 
     try{
+        console.log(req.query);
         if (states.includes(req.query.state)){
-            item = await findOne(items,{'id':req.query.idItem});
-            const filter = {'id':req.query.idWatchlist};
-            watchlist = await findOne(watchlists,filter);
-            const update = {$push:{'array-items':{'id':item.id,'state':req.query.state}}};
-            updatedResult = await updateOne(watchlists,watchlist,update);
-            return res.status(200).json(await findOne(watchlists,{'id':req.query.idWatchlist}));
+            if (req.query.idItem && req.query.idWatchlist){
+                item = await findOne(items,{'id':req.query.idItem});
+                const filter = {'id':req.query.idWatchlist};
+                watchlist = await findOne(watchlists,filter);
+                const update = {$push:{'array-items':{'id':item.id,'state':req.query.state}}};
+                updatedResult = await updateOne(watchlists,watchlist,update);
+                return res.status(200).json(await findOne(watchlists,{'id':req.query.idWatchlist}));
+            }
+            else if (req.query.titleItem && req.query.nomWatchlist){
+                item = await findOne(items,{'title':req.query.titleItem});
+                const filter = {'nom':req.query.nomWatchlist}
+                // console.log(filter);
+                watchlist = await findOne(watchlists,filter);
+                const update = {$push:{'array-items':{'id':item.id,'state':req.query.state}}};
+                updatedResult = await updateOne(watchlists,watchlist,update);
+                // console.log(updatedResult)
+                return res.status(200).json(await findOne(watchlists,{'id':watchlist.id}));
+            }
         }
         return res.status(200).send('L etat n existe pas, conflits');
     }
